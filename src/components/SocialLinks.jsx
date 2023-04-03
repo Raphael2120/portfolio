df753ff03186f363245cfcd9fa665c54dc46dbbb
Raphael2120 <73787206+Raphael2120@users.noreply.github.com>
2023-04-03 07:58:19 +0200

    modifying sociallinks

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -22,7 +22,7 @@ const SocialLinks = () => {
           GitHub <FaGithub size={30} />
         </>
       ),
-      href: "https://github.com/theyashpatel",
+      href: "https://github.com/Raphael2120",
     },
     {
       id: 3,
